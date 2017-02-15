73e07c84033503e0edc0ac7fb011711360e2d51e
Juan <mrjuancod94@gmail.com>
2017-02-15 15:50:54 +0100

    1.0.0 TP1_EV2

---
+++ src/main/java/ProyectoTP1/CalculoMedia/CalculoMedia.java
@@ -0,0 +1,16 @@
+package ProyectoTP1.CalculoMedia;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+		// Programa que calcule la media de tres números.
+
+				int media;
+				int a=3;
+				int b=9;
+				int c=13;
+				
+				media=a+b+c/3;
+				System.out.println("La media de "+a+", "+b+" y "+c+" es "+media);
+	}
+}
